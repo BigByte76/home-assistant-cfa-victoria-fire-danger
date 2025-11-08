45749a23139916a7df4b94dfbebe2cb114ead20e
BigByte76 <60337336+BigByte76@users.noreply.github.com>
2025-11-08 22:11:17 +1100

    Create config_flow.py

---
+++ custom_components/config_flow.py
@@ -0,0 +1,16 @@
+import voluptuous as vol
+from homeassistant import config_entries
+
+from .const import DOMAIN, DISTRICTS
+
+class CFAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title=f"CFA {user_input['district']}", data=user_input)
+        schema = vol.Schema({
+            vol.Required("district"): vol.In(DISTRICTS)
+        })
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
