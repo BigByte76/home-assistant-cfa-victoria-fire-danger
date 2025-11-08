fddb717fc4cb188f1d665a425f9a2274fd02378f
BigByte76 <60337336+BigByte76@users.noreply.github.com>
2025-11-08 22:11:59 +1100

    Create const.py

---
+++ custom_components/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "cfa_victoria_fire_danger"
+
+DISTRICTS = [
+    "Central", "East Gippsland", "Mallee", "North Central",
+    "North East", "Northern Country", "South West", "West and South Gippsland", "Wimmera"
+]
